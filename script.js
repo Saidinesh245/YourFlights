cc2face0361e656a0e11d3c559eb9f50149a770e
Saidinesh245 <dineshsai841@gmail.com>
2024-07-24 21:21:59 -0400

    Files copied.

---
+++ script.js
@@ -0,0 +1,23 @@
+document.addEventListener('DOMContentLoaded', function () {
+    var oneWayRadioButton = document.getElementById("one-way");
+    var roundRadioButton = document.getElementById("round-trip");
+    var arriveBox = document.getElementById("arrive-input-box");
+
+    oneWayRadioButton.addEventListener("click", function () {
+        arriveBox.style.display = "none";
+    });
+
+    roundRadioButton.addEventListener("click", function() {
+        arriveBox.style.display = "";
+    })
+
+});
+
+function swapAirports(){
+    var departingAirport = document.getElementById("departing-airport");
+    var arrivalAirport = document.getElementById("arrival-airport");
+
+    var tempValue = departingAirport.value;
+    departingAirport.value = arrivalAirport.value;
+    arrivalAirport.value = tempValue; 
+}
